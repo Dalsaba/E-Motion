72427bf40e5b81c445654d4c463c99d9cd50d15b
Lucy <lucycorreia-lopes@Lucys-MacBook-Pro.local>
2022-09-20 19:03:48 +0200

    save point before checkout front

--- migrations/Version20220920163503.php
+++ migrations/Version20220920163503.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220919075007 extends AbstractMigration
+final class Version20220920163503 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,11 @@ final class Version20220919075007 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, date_naissance DATE NOT NULL, adresse VARCHAR(255) NOT NULL, telephone INT NOT NULL, numero_permis VARCHAR(15) NOT NULL, UNIQUE INDEX UNIQ_C7440455E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, client_id_id INT NOT NULL, date_de_debut DATE NOT NULL, date_de_fin DATE NOT NULL, prix INT NOT NULL, statut VARCHAR(255) NOT NULL, INDEX IDX_5E9E89CBDC2902E0 (client_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE location_vehicule (location_id INT NOT NULL, vehicule_id INT NOT NULL, INDEX IDX_F87ADDFF64D218E (location_id), INDEX IDX_F87ADDFF4A4A3511 (vehicule_id), PRIMARY KEY(location_id, vehicule_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE vehicule (id INT AUTO_INCREMENT NOT NULL, classe_id INT DEFAULT NULL, immatricule VARCHAR(255) NOT NULL, marque VARCHAR(255) NOT NULL, modele VARCHAR(255) NOT NULL, num_serie VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, nb_kilometre INT NOT NULL, date_achat DATETIME NOT NULL, prix_achat INT NOT NULL, INDEX IDX_292FFF1D8F5EA509 (classe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE vehicule_classe (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, prix INT NOT NULL, price_id VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, date_naissance DATE NOT NULL, adresse VARCHAR(255) NOT NULL, telephone INT NOT NULL, numero_permis VARCHAR(15) NOT NULL, UNIQUE INDEX UNIQ_C7440455E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, client_id_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', date_de_debut DATE NOT NULL, date_de_fin DATE NOT NULL, prix INT NOT NULL, statut VARCHAR(255) NOT NULL, INDEX IDX_5E9E89CBDC2902E0 (client_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE location_vehicule (location_id INT NOT NULL, vehicule_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', INDEX IDX_F87ADDFF64D218E (location_id), INDEX IDX_F87ADDFF4A4A3511 (vehicule_id), PRIMARY KEY(location_id, vehicule_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE vehicule (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', classe_id INT DEFAULT NULL, immatricule VARCHAR(255) NOT NULL, marque VARCHAR(255) NOT NULL, modele VARCHAR(255) NOT NULL, num_serie VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, nb_kilometre INT NOT NULL, date_achat DATETIME NOT NULL, prix_achat INT NOT NULL, INDEX IDX_292FFF1D8F5EA509 (classe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE vehicule_classe (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, prix INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBDC2902E0 FOREIGN KEY (client_id_id) REFERENCES client (id)');
         $this->addSql('ALTER TABLE location_vehicule ADD CONSTRAINT FK_F87ADDFF64D218E FOREIGN KEY (location_id) REFERENCES location (id) ON DELETE CASCADE');
